d489aa05f3af3d3ee7eb91d14665ce14b17a41c7
xujl1062 <5034667@qq.com>
2015-09-25 17:11:18 +0800

    2015-9-25 17:11

--- src/main/java/org/smart4j/framework/helper/ClassHelper.java
+++ src/main/java/org/smart4j/framework/helper/ClassHelper.java
@@ -3,6 +3,8 @@ package org.smart4j.framework.helper;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.smart4j.framework.annotation.Action;
+import org.smart4j.framework.annotation.Service;
 /**
  * 类操作助手
  *
@@ -27,8 +29,42 @@ public final class ClassHelper {
         return CLASS_SET;
     }
 
+    /**
+     * 获取所有应用包下的 Service 类
+     * @return
+     */
     public static Set<Class<?>> getServiceClassSet(){
         Set<Class<?>> classSet = new HashSet<Class<?>>();
-        return null;
+        for (Class<?> cls : CLASS_SET){
+            if (cls.isAnnotationPresent(Service.class)){
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包下所有的 Controller 类
+     * @return
+     */
+    public static Set<Class<?>> getControllerClassSet(){
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET){
+            if (cls.isAnnotationPresent(Action.class)){
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包下所有的 Bean 类（包括：Service、Controller 等）
+     * @return
+     */
+    public static Set<Class<?>> getBeanClassSet(){
+        Set<Class<?>> beanCLassSet = new HashSet<Class<?>>();
+        beanCLassSet.addAll(getServiceClassSet());
+        beanCLassSet.addAll(getControllerClassSet());
+        return beanCLassSet;
     }
 }
